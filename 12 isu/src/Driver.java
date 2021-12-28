c36a53b34a6ad5438170158fa26c778ea245768b
pranav <pranavsworkemail@gmail.com>
2021-12-28 03:36:08 -0500

    Update Driver.java

--- 12 isu/src/Driver.java
+++ 12 isu/src/Driver.java
@@ -1,12 +1,29 @@
+import java.awt.Dimension;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Driver {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Driver extends JPanel{
+	
+	static JFrame frame;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		frame = new JFrame ("Pokemon");
+		Driver panel = new Driver();
+		panel.setPreferredSize(new Dimension(1920, 1080));
+		frame.add(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+		
 		
 		try {
 			Pokemon.addAllPokemon();
