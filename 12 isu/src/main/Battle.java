f68ad023de2f90f2359a9c284752edb9e7765575
stanleyylin <stanleylin82@gmail.com>
2022-01-14 19:57:21 -0500

    still battle, main buttons work

--- 12 isu/src/main/Battle.java
+++ 12 isu/src/main/Battle.java
@@ -5,12 +5,13 @@ import java.awt.*;
 import getimages.LoadImage;
 import pokesetup.Pokemon;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.*;
 import javax.swing.Timer;
+import javax.swing.text.Style;
+import javax.swing.text.StyleContext;
 
 import java.awt.image.*;
 import java.io.File;
@@ -19,7 +20,7 @@ import java.io.InputStream;
 import java.util.*;
 import java.awt.font.TextAttribute;
 
-public class Battle extends JPanel implements MouseListener {
+public class Battle extends JPanel {
 	
 	BufferedImage background; // Background image
 	Font font;
@@ -36,10 +37,14 @@ public class Battle extends JPanel implements MouseListener {
 	static boolean timerOn; // Is the timer on?
 	static int counter;
 	static float[] opacity;
+	String message;
+	
+	static Button[] buttons1;
 	
 	public Battle()
 	{
 		setPreferredSize(new Dimension(Driver2.screenWidth, Driver2.screenHeight));
+		setLayout(null);
 	    setBackground(Color.BLACK);
 		LoadImage loader = new LoadImage();
 		battleStats = new BufferedImage[4];
@@ -68,7 +73,7 @@ public class Battle extends JPanel implements MouseListener {
 			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("res/PokemonGb-RAeo.ttf")));
 //			Map<TextAttribute, Object> attributes = new HashMap<TextAttribute, Object>();
 //			attributes.put(TextAttribute.TRACKING, -0.1);
-			font = new Font("Pokemon GB", Font.PLAIN, 20);
+			font = new Font("Pokemon GB", Font.PLAIN, 22);
 //			font = font.deriveFont(attributes);
 		} 
 		catch (FontFormatException e) 
@@ -80,10 +85,54 @@ public class Battle extends JPanel implements MouseListener {
 			e.printStackTrace();
 		}
 		playerTurn = false;
-		timer = new Timer(35, new TimerEventHandler ());
+		timer = new Timer(35, new TimerEventHandler());
 		timerOn = false;
-		repaint();
 	
+//		bottomBar = new JPanel();
+//		bottomBar.setPreferredSize(new Dimension(1080, 134));
+//		bottomBar.setLayout(null);
+//		bottomBar.setBackground(new Color(0f, 0f, 0f, 0f));
+		buttons1 = new Button[4];
+		BufferedImage buttonSheet = null;
+		BufferedImage temp1;
+		BufferedImage temp2;
+		
+		try
+		{
+			buttonSheet = loader.loadImage("res/battle/battlebuttons.png");
+		}
+		catch(IOException e) {}
+		
+		// Fight Button
+		temp1 = loader.resize(buttonSheet.getSubimage(26, 0, 74, 46), 160, 100);
+		temp2 = loader.resize(buttonSheet.getSubimage(158, 0, 74, 46), 160, 100);
+		buttons1[0] = new Button(temp1, temp2, 160, 100);
+		buttons1[0].setBounds(59, 595, 160, 100);
+		add(buttons1[0]);
+		
+		// Pokemon button
+		temp1 = loader.resize(buttonSheet.getSubimage(14, 46, 102, 47), 220, 101);
+		temp2 = loader.resize(buttonSheet.getSubimage(145, 46, 102, 47), 220, 101);
+		buttons1[1] = new Button(temp1, temp2, 220, 101);
+		buttons1[1].setBounds(320, 595, 220, 101);
+		add(buttons1[1]);
+		
+		// Bag button
+		temp1 = loader.resize(buttonSheet.getSubimage(32, 96, 88, 41), 190, 89);
+		temp2 = loader.resize(buttonSheet.getSubimage(164, 96, 88, 41), 190, 89);
+		buttons1[2] = new Button(temp1, temp2, 190, 89);
+		buttons1[2].setBounds(650, 601, 190, 89);
+		add(buttons1[2]);
+		
+		// Run button
+		temp1 = loader.resize(buttonSheet.getSubimage(33, 143, 60, 44), 129, 95);
+		temp2 = loader.resize(buttonSheet.getSubimage(162, 143, 60, 44), 129, 95);
+		buttons1[3] = new Button(temp1, temp2, 129, 95);
+		buttons1[3].setBounds(894, 606, 129, 95);
+		add(buttons1[3]);
+		
+//		bottomBar.setBounds(0, 586, 1080, 134);
+//		add(bottomBar);
 	}
 	
 	// TimeEventHandler class is for the timer.
@@ -96,26 +145,37 @@ public class Battle extends JPanel implements MouseListener {
 			repaint();
 		}
 	}
-		
-	public void mouseClicked(MouseEvent e) 
-	{	
+	
+	public static void hideButtons()
+	{
+		for(Button b : buttons1)
+		{
+			b.setIcon(null);
+			b.displayed(false);
+		}	
+	}
+	
+	public void updateButtons()
+	{
 		
 	}
-
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-
-	public void updateText(Graphics g)
+	public void updateText(Graphics2D g)
 	{
+		
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.BLACK);
-		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f));
-		g2.fillRect(0, 586, 1080, 134);
 		g2.setFont(font);
 		g2.setColor(Color.WHITE);
-		g2.drawString("Pranav wants to throw hands.", 69, 648);
+		g2.drawString("This is a message.", 69, 648);
+	}
+	
+	public void updateStats(Graphics2D g)
+	{
+		
+	}
+	
+	public void loadMons(Graphics2D g)
+	{
+		
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -123,8 +183,10 @@ public class Battle extends JPanel implements MouseListener {
 		g2.drawImage(background, 0, 0, null);
 		g2.drawImage(battleStats[0], 0, 110, null);
 		g2.drawImage(battleStats[1], Main.screenWidth-battleStats[1].getWidth(), 338, null);
-		
-		updateText(g);
+		g2.setColor(Color.BLACK);
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f));
+		g2.fillRect(0, 586, 1080, 134);
+		// updateText(g2);
 	}
 	
 	public void newBattle(Pokemon[] playerParty, Pokemon[] oppParty)
---
+++ 12 isu/src/main/Button.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Button extends JLabel implements MouseListener {
+	private ImageIcon unselected;
+	private ImageIcon selected;
+	private boolean displayed;
+	
+	public Button(BufferedImage unselected, BufferedImage selected, int width, int height)
+	{
+		this.unselected = new ImageIcon(unselected);
+		this.selected = new ImageIcon(selected);
+		displayed = true;
+		setPreferredSize(new Dimension(width, height));
+		setIcon(new ImageIcon(unselected));
+		// setBackground(new Color(0f, 0f, 0f, 0f));
+		addMouseListener(this);
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		Battle.hideButtons();
+	}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) 
+	{
+		if(displayed)
+			setIcon(selected);
+	}
+	public void mouseExited(MouseEvent e) {
+		if(displayed)
+			setIcon(unselected);
+	}
+	
+	public void displayed(boolean change)
+	{
+		displayed = change;
+	}
+}
