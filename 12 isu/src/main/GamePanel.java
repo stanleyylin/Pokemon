995c0e290ff779cdc8cba5bf83d2987f81792210
stanleyylin <stanleylin82@gmail.com>
2022-01-25 12:23:11 -0500

    hello

--- 12 isu/src/main/GamePanel.java
+++ 12 isu/src/main/GamePanel.java
@@ -76,7 +76,6 @@ public class GamePanel extends JPanel implements Runnable
 	    // MAPS
 	    
 	//1. Twinleaf City
-	    
 	    Rectangle[] collisions1 = new Rectangle[12];
 	    collisions1[0] = new Rectangle(0,0,520,120);
 	    collisions1[1] = new Rectangle(0,0,255,1131);
@@ -101,6 +100,7 @@ public class GamePanel extends JPanel implements Runnable
 		people1[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
 		
 	    Location twinLeaf = new Location("twinleaf2.png",collisions1,buildings1,people1,null);
+	    
 	    
 	//2.Sandgem City
 	    
@@ -133,9 +133,37 @@ public class GamePanel extends JPanel implements Runnable
 
 	    Location sandGem = new Location("sandgem.png",collisions2,buildings2,people2,null);
 
-	    //3. Jubilife City
+	 //3. Jubilife City
+	    Rectangle[] collisions3 = new Rectangle[14];
+	    collisions3[0] = new Rectangle(0,0,143*3,240*3);
+	    collisions3[0] = new Rectangle(143*3,0*3,300*3,112*3);
+	    collisions3[0] = new Rectangle(185*3,111*3,139*3,53*3);
+	    collisions3[0] = new Rectangle(185*3,162*3,15*3,32*3);
+	    collisions3[0] = new Rectangle(248*3,162*3,13*3,26*3);
+	    collisions3[0] = new Rectangle(310*3,162*3,13*3,26*3);
+	    collisions3[0] = new Rectangle(369*3,114*3,79*3,122*3);
+	    collisions3[0] = new Rectangle(447*3,0*3,252*3,91*3);
+	    collisions3[0] = new Rectangle(521*3,91*3,136*3,83*3);
+	    collisions3[0] = new Rectangle(786*3,0*3,270*3,91*3);
+	    collisions3[0] = new Rectangle(808*3,91*3,248*3,97*3);
+	    collisions3[0] = new Rectangle(808*3,188*3,79*3,52*3);
+	    collisions3[0] = new Rectangle(921*3,188*3,135*3,72*3);
+	    collisions3[0] = new Rectangle(921*3,306*3,134*3,288*3);
+	    collisions3[0] = new Rectangle(808*3,323*3,63*3,33*3);
+	    collisions3[0] = new Rectangle(808*3,356*3,12*3,24*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
+	    collisions3[0] = new Rectangle(0*3,0*3,0*3,0*3);
 	    
-	    //4. Oreburgh City
+	
+	    
+	    
+	 //4. Oreburgh City
 	    
 	    Rectangle[] collisions4 = new Rectangle[12];
 	    collisions4[0] = new Rectangle(350,0,1560,400);
@@ -151,8 +179,6 @@ public class GamePanel extends JPanel implements Runnable
 	    collisions4[10] = new Rectangle(2604,600,1000,1000);
 	    collisions4[11] = new Rectangle(2175,708,1000,70);
 
-
-	    
 	    Building[] buildings4 = new Building[3];
 		buildings4[0] = new Building(new Rectangle(1224, 834, 45, 28), new Rectangle(540, 543, 5, 1), new Person[0], new Rectangle[0], house1);//should b gym
 		buildings4[1] = new Building(new Rectangle(1380, 465, 45, 34), new Rectangle(540, 543, 5, 1), new Person[0], new Rectangle[0], pokecentre1);
@@ -163,8 +189,7 @@ public class GamePanel extends JPanel implements Runnable
 		people4[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
 		
 		Location oreburg = new Location("oreburgh.png",collisions4,buildings4,people4,null);
-
-	    //Floarama City
+	// 5: Floarama City
 		
 		Rectangle[] collisions5 = new Rectangle[9];
 		collisions5[0] = new Rectangle(0,900,1545,390);
@@ -177,7 +202,6 @@ public class GamePanel extends JPanel implements Runnable
 		collisions5[7] = new Rectangle(1110,1710,180,165);
 		collisions5[8] = new Rectangle(810,1410,270,210);
 
-
 	    Building[] buildings5 = new Building[3];
 		buildings5[0] = new Building(new Rectangle(930, 1605, 45, 60), new Rectangle(540, 543, 5, 1), new Person[0], new Rectangle[0], house1);//should b champion battle
 		buildings5[1] = new Building(new Rectangle(750, 2175, 45, 34), new Rectangle(540, 543, 5, 1), new Person[0], new Rectangle[0], pokecentre1);
@@ -186,12 +210,11 @@ public class GamePanel extends JPanel implements Runnable
 	    Person[] people5 = new Person[1];
 		people5[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
 
-
 		Location floaroma = new Location("floaroma.png",collisions5,buildings5,people5,null);
 
 		
 		
-	 // 10. Hearthome City:
+	// 6. Hearthome City:
 		// Collisions
 		// Use canva, multiply by 3, subtract the height by 30 and width by 10 or so
 		Rectangle[] collisions10 = new Rectangle[18];
@@ -232,27 +255,219 @@ public class GamePanel extends JPanel implements Runnable
 		//Grass: None
 		Location heartHome = new Location("hearthome.png", collisions10, buildings10, people10, null);
 		
+		// ROUTES
 	// Route 208
-		// Collisions
-		Rectangle[] collisions208 = new Rectangle[1];
-		collisions208[0] = new Rectangle(2286, 0, 787, 541); // leftmost up trees
-		// Buildings ----
+		Rectangle[] collisions208 = new Rectangle[21];
+		collisions208[0] = new Rectangle(0, 0, 217*3, 149*3);
+		collisions208[1] = new Rectangle(217*3, 84*3, 807*3, 118*3);
+		collisions208[2] = new Rectangle(0, 149*3, 88*3, 299*3); 
+		collisions208[3] = new Rectangle(88*3, 315*3, 246*3, 133*3);
+		collisions208[4] = new Rectangle(260*3, 175*3, 82*3, 83*3);
+		collisions208[5] = new Rectangle(391*3, 199*3, 59*3, 177*3);
+		collisions208[6] = new Rectangle(435*3, 118*3, 44*3, 81*3);
+		collisions208[7] = new Rectangle(568*3, 118*3, 14*3, 98*3);
+		collisions208[8] = new Rectangle(512*3, 331*3, 33*3, 43*3);
+		collisions208[9] = new Rectangle(449*3, 297*3, 105*3, 33*3);
+		collisions208[10] = new Rectangle(334*3, 364*3, 290*3, 84*3); 
+		collisions208[11] = new Rectangle(568*3, 215*3, 34*3, 31*3);
+		collisions208[12] = new Rectangle(580*3, 290*3, 35*3, 31*3); 
+		collisions208[13] = new Rectangle(628*3, 188*3, 35*3, 195*3);
+		collisions208[14] = new Rectangle(624*3, 385*3, 79*3, 21*3);
+		collisions208[15] = new Rectangle(624*3, 405*3, 400*3, 42*3);
+		collisions208[16] = new Rectangle(862*3, 358*3, 162*3, 47*3);
+		collisions208[17] = new Rectangle(961*3, 188*3, 63*3, 125*3);
+		collisions208[18] = new Rectangle(703*3, 179*3, 25*3, 63*3); 
+		collisions208[19] = new Rectangle(729*3, 116*3, 35*3, 126*3);
+		collisions208[20] = new Rectangle(763*3, 116*3, 261*3, 72*3);
+
 		Building[] buildings208 = new Building[1];
 		buildings208[0] = new Building(new Rectangle(2726, 574, 62, 56), new Rectangle(540, 543, 81, 32), new Person[0], new Rectangle[0], house1);
-		//-----
-		// People
+
 		Person[] people208 = new Person[0];
-		// Grass
+		people208[0] = null;
+
 		Rectangle[] grass208 = new Rectangle[1];
 		grass208[0] = new Rectangle(2007, 738, 480, 471);
-		Location T208 = new Location("208.png", collisions208, buildings208, people208, grass208);
+		
+		Location r208 = new Location("208.png", collisions208, buildings208, people208, null);
+	
+		
+		
+	// Route 207
+		Rectangle[] collisions207 = new Rectangle[16];
+		collisions207[0] = new Rectangle(0, 0, 56*3, 194*3);
+		collisions207[1] = new Rectangle(0, 194*3, 35*3, 227*3);
+		collisions207[2] = new Rectangle(35*3, 90*3, 169*3, 13*3);
+		collisions207[3] = new Rectangle(194*3, 104*3, 10*3, 100*3);
+		collisions207[4] = new Rectangle(204*3, 192*2, 10*3, 79*3);
+		collisions207[5] = new Rectangle(214*3, 261*3, 50*3, 10*3);
+		collisions207[6] = new Rectangle(308*3, 192*3, 39*3, 44*3);
+		collisions207[7] = new Rectangle(322*3, 236, 26*3, 75*3);
+		collisions207[8] = new Rectangle(257*3, 305, 52*3, 11*3);
+		collisions207[9] = new Rectangle(243*3, 339*3, 48*3, 77*3);
+		collisions207[10] = new Rectangle(291*3, 316*3, 192*3, 100*3);
+		collisions207[11] = new Rectangle(356*3, 273*3, 88*3, 44*3);
+		collisions207[12] = new Rectangle(318*3, 0, 706*3, 65*3);
+		collisions207[13] = new Rectangle(884*3, 65*3, 140*3, 354*3);
+		collisions207[14] = new Rectangle(744*3, 272*3, 140*3, 144*3);
+		collisions207[15] = new Rectangle(591*3, 316*3, 100*3, 153*3);
+		
+		Building[] buildings207 = new Building[0];
+
+		Person[] people207 = new Person[0];
+		// people10[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
+
+		Rectangle[] grass207 = new Rectangle[1];
+		grass207[0] = new Rectangle(49*3, 141*3, 132*3, 141*3);
+		Location r207 = new Location("207.png", collisions207, buildings207, people207, grass207);
+	
+	//  Route 203
+		Rectangle[] collisions203 = new Rectangle[15];
+		collisions203[0] = new Rectangle(0, 0, 82*3, 260*3);
+		collisions203[1] = new Rectangle(82*3, 0*3, 894*3, 86*3);
+		collisions203[2] = new Rectangle(82*3, 86*3, 227*3, 15*3);
+		collisions203[3] = new Rectangle(309*3, 86*3, 79*3, 44*3);
+		collisions203[4] = new Rectangle(388*3, 86*3, 100*3, 54*3);
+		collisions203[5] = new Rectangle(906*3, 86*3, 14*3, 44*3);
+		collisions203[6] = new Rectangle(920*3, 86*3, 56*3, 282*3);
+		collisions203[7] = new Rectangle(906*3, 163*3, 14*3, 97*3);
+		collisions203[8] = new Rectangle(614*3, 260*3, 307*3, 108*3);
+		collisions203[9] = new Rectangle(394*3, 296*3, 219*3, 72*3);
+		collisions203[10] = new Rectangle(0, 320*3, 394*3, 48*3);
+		collisions203[11] = new Rectangle(155*3, 144*3, 26*3, 87*3);
+		collisions203[12] = new Rectangle(241*3, 144*3, 49*3, 81*3);
+		collisions203[13] = new Rectangle(462*3, 206*3, 86*3, 36*3);
+		collisions203[14] = new Rectangle(796*3, 187*3, 27*3, 37*3);
+
+		Building[] buildings203 = new Building[0];
+
+		Person[] people203 = new Person[0];
+		// people10[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
+
+		Rectangle[] grass203 = new Rectangle[4];
+		grass203[0] = new Rectangle(90*3, 164*3, 60*3, 72*3);
+		grass203[1] = new Rectangle(438*3, 256*3, 144*3, 37*3);
+		grass203[2] = new Rectangle(618*3, 94*3, 172*3, 67*3);
+		grass203[3] = new Rectangle(680*3, 185*3, 138*3, 42*3);
+		
+		Location r203 = new Location("hearthome.png", collisions203, buildings203, people203, null);
+	
+		
+		
+	//  Route 204
+		Rectangle[] collisions204 = new Rectangle[16];
+		collisions204[0] = new Rectangle(0, 0, 151*3, 530*3);
+		collisions204[1] = new Rectangle(0, 530*3, 24*3, 156*3);
+		collisions204[2] = new Rectangle(24*3, 530*3, 35*3, 67*3);
+		collisions204[3] = new Rectangle(0, 686*3, 182*3, 136*3);
+		collisions204[4] = new Rectangle(213*3, 32*3, 174*3, 64*3);
+		collisions204[5] = new Rectangle(386*3, 32*3, 121*3, 404*3);
+		collisions204[6] = new Rectangle(353*3, 64*3, 33*3, 63*3);
+		collisions204[7] = new Rectangle(208*3, 142*3, 55*3, 83*3);
+		collisions204[8] = new Rectangle(263*3, 165*3, 37*3, 55*3);
+		collisions204[9] = new Rectangle(208*3, 280*3, 22*3, 24*3);
+		collisions204[10] = new Rectangle(208*3, 304*3, 175*3, 32*3);
+		collisions204[11] = new Rectangle(361*3, 256*3, 26*3, 48*3);
+		collisions204[12] = new Rectangle(353*3, 336*3, 33*3, 48*3);
+		collisions204[13] = new Rectangle(204*3, 401*3, 22*3, 35*3);
+		collisions204[14] = new Rectangle(226*3, 401*3, 36*3, 241*3);
+		collisions204[15] = new Rectangle(263*3, 401*3, 242*3, 429*3);
+
+		Building[] buildings204 = new Building[0];
+		
+		Person[] people204 = new Person[0];
+		// people10[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
+
+		Rectangle[] grass204 = new Rectangle[4];
+		grass204[0] = new Rectangle(158*3, 51*3, 47*3, 73*3);
+		grass204[1] = new Rectangle(300*3, 165*3, 83*3, 59*3);
+		grass204[2] = new Rectangle(59*3, 537*3, 161*3, 104*3);
+		grass204[3] = new Rectangle(29*3, 646*3, 71*3, 35*3);
+		
+		Location r204 = new Location("204.png", collisions204, buildings204, people204, null);
+		
+		
+		
+	//  Route 201
+		Rectangle[] collisions201 = new Rectangle[12];
+		collisions201[0] = new Rectangle(0, 0, 674*3, 127*3);
+		collisions201[1] = new Rectangle(674*3, 0, 366*3, 78*3);
+		collisions201[2] = new Rectangle(77*3, 129*3, 53*3, 24*3);
+		collisions201[3] = new Rectangle(296*3, 129*3, 90*3, 24*3);
+		collisions201[4] = new Rectangle(591*3, 129*3, 53*3, 28*3);
+		collisions201[5] = new Rectangle(826*3, 116*3, 31*3, 75*3);
+		collisions201[6] = new Rectangle(0, 245*3, 130*3, 24*3);
+		collisions201[7] = new Rectangle(0, 269*3, 230*3, 83*3);
+		collisions201[8] = new Rectangle(331*3, 245*3, 154*3, 25*3);
+		collisions201[9] = new Rectangle(297*3, 270*3, 438*3, 83*3);
+		collisions201[10] = new Rectangle(734*3, 294*3, 206*3, 58*3);
+		collisions201[11] = new Rectangle(940*3, 137*3, 100*3, 215*3);
+
+		Building[] buildings201 = new Building[0];
+
+		Person[] people201 = new Person[0];
+		// people10[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
+
+		Rectangle[] grass201 = new Rectangle[2];
+		grass201[0] = new Rectangle(331*3, 215*3, 154*3, 27*3);
+		grass201[1] = new Rectangle(680*3, 89*3, 139*3, 120*3);
+		
+		Location r201 = new Location("201.png", collisions201, buildings201, people201, null);
+	
+	//  Route 202
+		Rectangle[] collisions202 = new Rectangle[15];
+		collisions202[0] = new Rectangle(0, 0, 162*3, 88*3);
+		collisions202[1] = new Rectangle(0, 88*3, 61*3, 264*3);
+		collisions202[2] = new Rectangle(162*3, 29*3, 27*3, 59*3);
+		collisions202[3] = new Rectangle(179*3, 0, 60*3, 20*3);
+		collisions202[4] = new Rectangle(290*3, 0, 60*3, 20*3);
+		collisions202[5] = new Rectangle(327*3, 0, 188*3, 88*3);
+		collisions202[6] = new Rectangle(515*3, 29*3, 95*3, 452*3);
+		collisions202[7] = new Rectangle(61*3, 155*3, 68*3, 58*3);
+		collisions202[8] = new Rectangle(129*3, 155*3, 27*3, 34*3);
+		collisions202[9] = new Rectangle(61*3, 155*3, 68*3, 58*3);
+		collisions202[10] = new Rectangle(243*3, 138*3, 123*3, 139*3);
+		collisions202[11] = new Rectangle(129*3, 270*3, 27*3, 84*3); // 
+		collisions202[12] = new Rectangle(244*3, 315*3, 121*3, 38*3);
+		collisions202[13] = new Rectangle(61*3, 402*3, 288*3, 50*3);
+		collisions202[14] = new Rectangle(349*3, 421*3, 65*3, 31*3);
+		
+		Building[] buildings202 = new Building[0];
+
+		Person[] people202 = new Person[0];
+		// people10[0] = new Person(new Rectangle(665, 922, 51, 72), "down", "nurse.png", 17, 24, "d1.txt");
+
+		Rectangle[] grass202 = new Rectangle[4];
+		grass202[0] = new Rectangle(81*3, 97*3, 75*3, 53*3);
+		grass202[1] = new Rectangle(366*3, 97*3, 55*3, 69*3);
+		grass202[2] = new Rectangle(382*3, 190*3, 133*3, 80*3);
+		grass202[3] = new Rectangle(61*3, 286*3, 68*3, 116*3);
+		
+		Location r202 = new Location("hearthome.png", collisions203, buildings203, people203, null);
+
+		// GATES
+		
+//	// Route 208
+//		// Collisions
+//		Rectangle[] collisions208 = new Rectangle[1];
+//		collisions208[0] = new Rectangle(2286, 0, 787, 541); // leftmost up trees
+//		// Buildings ----
+//		Building[] buildings208 = new Building[1];
+//		buildings208[0] = new Building(new Rectangle(2726, 574, 62, 56), new Rectangle(540, 543, 81, 32), new Person[0], new Rectangle[0], house1);
+//		//-----
+//		// People
+//		Person[] people208 = new Person[0];
+//		// Grass
+//		Rectangle[] grass208 = new Rectangle[1];
+//		grass208[0] = new Rectangle(2007, 738, 480, 471);
+//		Location T208 = new Location("208.png", collisions208, buildings208, people208, grass208);
 	
 		// {0, hearthome.getHeight()-screenHeight, }
 
-	// Gate: 11-HeartHome and 208
-		Gate gate11 = new Gate(0, T208.getBG().getWidth()-screenWidth, T208.getBG().getHeight()-screenHeight, heartHome, new Rectangle(283, 1973, 54, 109), T208, new Rectangle(3021, 975, 52, 93));
-		heartHome.addGate(gate11);
-		T208.addGate(reverseGate(gate11, 0, heartHome.getBG().getHeight()-screenHeight));		
+//	// Gate: 11-HeartHome and 208
+//		Gate gate11 = new Gate(0, T208.getBG().getWidth()-screenWidth, T208.getBG().getHeight()-screenHeight, heartHome, new Rectangle(283, 1973, 54, 109), T208, new Rectangle(3021, 975, 52, 93));
+//		heartHome.addGate(gate11);
+//		T208.addGate(reverseGate(gate11, 0, heartHome.getBG().getHeight()-screenHeight));		
 		
 		// Functionalities
 		dialogue = new Dialogue(this, player);
